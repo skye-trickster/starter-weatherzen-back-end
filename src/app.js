9a55af36740d51916d456ddae6ba3b0358088dc0
Vincent Mills <vincentmills273@gmail.com>
2022-05-06 08:29:43 -0400

    Step 2: App.js router works
    - able to create observations using app.post
    - unable to GET observations at this time. That's the next step.

--- src/app.js
+++ src/app.js
@@ -9,6 +9,18 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+let nextId = 1;
+
+// don't create the controller and router just yet. this is here at this step for testing to ensure that the comunication works before going too deep
+app.post('/observations', ((request, response) => {
+    const newObservation = request.body.data;
+
+    newObservation.observation_id = nextId++
+
+    response.status(201).json({
+        data: newObservation,
+    })
+}))
 
 app.use(notFound);
 app.use(errorHandler);
